c8833b4bcfbb9ef8f83e6793668ee502597e2074
Admin <reteamboss@gmail.com>
2024-08-31 15:03:08 +0300

    Задание 3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,14 @@
+import org.jetbrains.annotations.NotNull
+
 fun main() {
-    println("Hello World!")
+    var array = arrayOf(1,23,234,0,6,7,4,3,5,0,45,56,)
+    var a = array.indexOfFirst { it == 0 }
+    var b = array.indexOfLast { it == 0 }
+    var count = 0
+
+    for (num in a..b){
+        count+=1
+    }
+    println(count-2)
+
 }
